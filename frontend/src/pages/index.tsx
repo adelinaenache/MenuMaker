d6782460922d4de0c4872033b09450aa18bc287a
Alexandru Vasilescu <alexandru.vasilescu01@gmail.com>
2021-05-17 06:33:48 +0300

    fix: home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,15 @@
 import { Hero, Layout, Main } from '@/components';
-import { Text } from '@chakra-ui/react';
 import { useUser } from '@/hooks';
+import { Text } from '@chakra-ui/layout';
 
 const Index = () => {
   const { user } = useUser();
 
-  console.log(user);
-
   return (
-    <Layout>
+    <Layout unwrapped>
       <Hero title="MenuMaker" />
 
-      <Main />
+      <Main>{user && <Text>Hello, {user.email}</Text>}</Main>
     </Layout>
   );
 };
