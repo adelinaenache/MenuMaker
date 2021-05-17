6736fb00abba451f5e3ac89d7b30f7a00f188389
Alexandru Vasilescu <alexandru.vasilescu01@gmail.com>
2021-05-17 04:38:47 +0300

    feat: restore login redirection on unauthorized access

--- frontend/src/HOC/withAuth.tsx
+++ frontend/src/HOC/withAuth.tsx
@@ -1,19 +1,23 @@
 import { Spinner } from '@chakra-ui/spinner';
 import { useUser } from '@/hooks';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export function withAuth<T>(WrappedComponent: React.ComponentType<T>) {
   return (props: T) => {
     const { loading, error } = useUser();
+    const router = useRouter();
 
     if (loading) {
       return <Spinner size="xl" />;
     }
 
     if (error) {
-      // router.replace("auth/login");
-      console.error(error);
-      return null;
+      router.replace('/auth/login');
+    }
+
+    if (loading || error) {
+      return <Spinner size="xl" />;
     }
 
     return <WrappedComponent {...props} />;
