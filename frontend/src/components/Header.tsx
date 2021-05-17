b9e8f2b3e530b8b7b237f825c41f1abc14ce2fa6
Alexandru Vasilescu <alexandru.vasilescu01@gmail.com>
2021-05-17 06:29:40 +0300

    feat: add logo and interactivity to the header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,24 @@
-import { DarkModeSwitch } from '@/components';
-import { Stack } from '@chakra-ui/react';
+import { Button, DarkModeSwitch } from '@/components';
+import { NextChakraLink } from '@/components/NextChakraLink';
+import { Flex, HStack, Text } from '@chakra-ui/react';
 import React from 'react';
 
 export const Header = () => {
   return (
-    <Stack w="100%" alignItems="flex-end" p={4}>
-      <DarkModeSwitch />
-    </Stack>
+    <HStack w="100%" justifyContent="space-between" p={4}>
+      <HStack>
+        <Flex justifyContent="center" bgGradient="linear(to-l, #7928CA, #FF0080)" bgClip="text">
+          <NextChakraLink href="/">
+            <Text fontSize="xl">MenuMaker</Text>
+          </NextChakraLink>
+        </Flex>
+      </HStack>
+
+      <HStack spacing={8}>
+        <Button href="/restaurants">View your restaurants</Button>
+
+        <DarkModeSwitch />
+      </HStack>
+    </HStack>
   );
 };
