def446be8f5bd2aea2bcf40cd881063c996f18fd
Alexandru Vasilescu <alexandru.vasilescu01@gmail.com>
2021-06-16 14:10:45 +0300

    feat: improve your own restaurants page

--- frontend/src/pages/restaurants/index.tsx
+++ frontend/src/pages/restaurants/index.tsx
@@ -1,26 +1,75 @@
 import { Button, Layout } from '@/components';
-import { NextChakraLink } from '@/components/NextChakraLink';
+import { Price } from '@/components/Price';
 import { withAuth } from '@/hoc';
-import { useMyRestaurantList } from '@/hooks';
-import { Text } from '@chakra-ui/react';
+import { useMyRestaurantList, useUser } from '@/hooks';
+import { Box, Flex, Image, SimpleGrid, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const RestaurantsDashboard = withAuth(() => {
   const { restaurants } = useMyRestaurantList();
+  const router = useRouter();
+  const { user } = useUser();
 
   return (
     <Layout>
+      <Text fontSize="3xl" textAlign="center" mb={10}>
+        You are currently logged in as: {user?.email}
+      </Text>
+
       {restaurants.length === 0 && <Text>You have no restaurants.</Text>}
 
-      {restaurants.map((restaurant, index) => (
-        <NextChakraLink href={`/restaurants/${restaurant.id}`} key={index}>
-          {restaurant.name}
-        </NextChakraLink>
-      ))}
+      <SimpleGrid columns={3} gap={6} mt={10}>
+        {restaurants.map((restaurant) => (
+          <Box
+            maxW="sm"
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            onClick={async () => {
+              await router.push(`/restaurants/${restaurant.id}`);
+            }}
+            key={restaurant.id}
+            cursor="pointer"
+          >
+            <Image
+              h="200px"
+              w="100%"
+              objectFit="cover"
+              src={'http://pizzeriaitalianasacele.ro/wp-content/uploads/2014/09/margheritawiki.jpg'}
+              alt={'Pizza'}
+            />
 
-      <Button href="/restaurants/new" w={40}>
-        Add a restaurant
-      </Button>
+            <Box p="6">
+              <Box d="flex" alignItems="baseline">
+                <Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase">
+                  {restaurant.itemCount} items
+                </Box>
+              </Box>
+
+              <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+                {restaurant.name}
+              </Box>
+
+              <Box>
+                {restaurant.priceRange[1] !== 0 && (
+                  <Flex>
+                    <Price value={restaurant.priceRange[0]} />
+                    <Box px={1}>-</Box>
+                    <Price value={restaurant.priceRange[1]} />
+                  </Flex>
+                )}
+              </Box>
+            </Box>
+          </Box>
+        ))}
+      </SimpleGrid>
+
+      <Flex justifyContent="center" py={10}>
+        <Button w={60} href="/restaurants/new">
+          Add new restaurant
+        </Button>
+      </Flex>
     </Layout>
   );
 });
